6f3653d3c6501f7b473a4ace29336f82191ebd20
KennyDove <addison.dove@gmail.com>
2020-05-10 23:42:55 -0400

    Proper QUICK DB

--- EmployeeSQL/Quick database files/QuickDBD-export.sql
+++ EmployeeSQL/Quick database files/QuickDBD-export.sql
@@ -3,8 +3,8 @@
 
 
 CREATE TABLE "Departments" (
-    "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar(30)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
         "dept_no"
      )
@@ -12,11 +12,11 @@ CREATE TABLE "Departments" (
 
 CREATE TABLE "Dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
+    "dept_no" varchar(30)   NOT NULL
 );
 
 CREATE TABLE "Dept_manager" (
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar(30)   NOT NULL,
     "emp_no" int   NOT NULL
 );
 
@@ -26,7 +26,7 @@ CREATE TABLE "Employees" (
     "birth_date" date   NOT NULL,
     "first_name" varchar(30)   NOT NULL,
     "last_name" varchar(30)   NOT NULL,
-    "sex" varchar(3)   NOT NULL,
+    "sex" char   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
@@ -39,12 +39,12 @@ CREATE TABLE "Salaries" (
 );
 
 CREATE TABLE "Titles" (
-    "title_id" int   NOT NULL,
-    "title" varchar(30)   NOT NULL
+    "title_id" varchar(20)   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
 );
-
-ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
 
 ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
@@ -58,9 +58,9 @@ REFERENCES "Departments" ("dept_no");
 ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
